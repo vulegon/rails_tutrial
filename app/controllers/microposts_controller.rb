f18b8fe5a0e2de8dfb6322af7b6b275c51711a19
vulegon <walkurepqrt@gmail.com>
2022-06-21 09:56:58 +0900

    13章終了

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,6 +3,7 @@ class MicropostsController < ApplicationController
   before_action :correct_user,   only: :destroy
   def create
     @micropost=current_user.microposts.build(micropost_params)
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
@@ -21,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-    params.require(:micropost).permit(:content)
+    params.require(:micropost).permit(:content, :image)
   end
 
   def correct_user
